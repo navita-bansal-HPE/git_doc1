2b7294e1597755189d205920c1182dc357ccaf61
navita-bansal-HPE <navita.bansal-ext@hpe.com>
2025-02-19 15:04:02 +0530

    mm

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,15 +13,22 @@ author = 'navi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "sidebarwidth": 400,  # Adjust the left sidebar width here
+}
+
+html_static_path = ["_static"]
