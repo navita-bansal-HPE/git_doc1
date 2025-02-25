b03e0ad068646dce0f6ce4d9b3ccaac0cebcda9c
navita-bansal-HPE <navita.bansal-ext@hpe.com>
2025-02-25 14:01:31 +0530

    test23

---
+++ automation_libs/automation_libs_1/test2/newtest2.py
@@ -0,0 +1,26 @@
+"""To print the message"""
+
+class Printer:
+    """
+    A simple class to print a message.
+    """
+
+    def __init__(self, message):
+        """
+        Initializes the Printer class with a message.
+
+        :param message: str, the message to be printed.
+        """
+        self.message = message
+
+    def display(self):
+        """
+        Prints the stored message.
+        """
+        print(self.message)
+
+
+# Example usage
+if __name__ == "__main__":
+    printer = Printer("Hello, this is a Python class with docstrings!!!")
+    printer.display()
