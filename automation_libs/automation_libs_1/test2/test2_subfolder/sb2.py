45f5bd92020741ef1ef9f834edcf6a051fa17246
navita-bansal-HPE <navita.bansal-ext@hpe.com>
2025-03-03 16:40:10 +0530

    test30

---
+++ automation_libs/automation_libs_1/test2/test2_subfolder/sb2.py
@@ -0,0 +1,23 @@
+class Greeting:
+    """
+    A class to represent a greeting message.
+    """
+
+    def __init__(self, name):
+        """
+        Initialize the Greeting class with a name.
+
+        :param name: The name of the person to greet.
+        """
+        self.name = name
+
+    def say_hello(self):
+        """
+        Prints a greeting message.
+        """
+        print(f"Hello, {self.name}! Welcome to the world of Python.")
+
+# Example usage
+if __name__ == "__main__":
+    greeting = Greeting("hello there, How r u")
+    greeting.say_hello()
